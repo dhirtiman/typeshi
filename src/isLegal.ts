7385be844eab3dc5249feae3c8266cfb8d665342
dhirtiman <dhirtimankhati@gmail.com>
2025-04-30 22:07:24 +0530

    stuff

--- src/isLegal.ts
+++ src/isLegal.ts
@@ -1,12 +1,30 @@
-function isLegal(age: number) {
-    if (age >= 18) {
+function isLegal(user: User) {
+    if (user.age >= 18) {
+        console.log(user.firstName + " is legal");
         return true;
     } else {
+        console.log(user.firstName + " is not legal");
         return false;
     }
 }
 
+interface User {
+    firstName: string;
+    lastName: string;
+    email?: string;
+    age: number;
+}
 
-console.log(isLegal(18));
-console.log(isLegal(17));
+const user: User = {
+    firstName: "dhirtiman",
+    lastName: "khati",
+    email: "dhirtimankhati@gmail.com",
+    age: 22,
+}
+
+console.log(user);
+
+
+
+console.log(isLegal(user));
 
